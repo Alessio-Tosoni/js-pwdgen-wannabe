024071b80da7d343d45678afbb18022799a33d88
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-10-25 16:23:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let Nome = prompt("inserisci il tuo nome")
+document.getElementById("nome").innerHTML= Nome
+
+let Cognome = prompt("inserisci il tuo cognome")
+document.getElementById("cognome").innerHTML= Cognome
+
+let Colore = prompt("inserisci il tuo colore preferito")
+document.getElementById("colore").innerHTML= Colore
+
+let pswd = ["nome" + "cognome" + "colore" + "x"]
+let x = "#23"
+
+document.getElementById = ["pswd"].innerHTML= "nome" + "cognome" + "colore" + "x"
